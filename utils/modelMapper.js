18187c9b35c1d7e7f5385cb00899d3ebf0c9ce02
AJMAL <ajmal@livelocal.cloud>
2025-09-02 19:35:25 +0530

    photo upload

--- utils/modelMapper.js
+++ utils/modelMapper.js
@@ -189,3 +189,10 @@ export const mapDealerDiscountEntityToResponse = (discount) => {
 
     return response;
 };
+
+export const formatFileSize = (bytes) => {
+    if (bytes === 0) return "0 Bytes";
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+    const i = Math.floor(Math.log(bytes) / Math.log(1024));
+    return parseFloat((bytes / Math.pow(1024, i)).toFixed(2)) + " " + sizes[i];
+};
